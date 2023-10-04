0a6f3135548a9a6cbdb14ab582ca702faa3864da
Михаил Иванов <mihailivanov@MacBook-Pro-Mihail.local>
2023-10-04 23:07:48 +0300

    add node-fetch lib, add http.service

--- src/services/env/index.ts
+++ src/services/env/index.ts
@@ -7,6 +7,7 @@ type EnvVariables = {
     TOKEN: string;
     METORING_TABLE_ID: string;
     MENTI_LIST_TABLE_ID: string;
+    USE_NATIVE_FETCH: boolean;
 };
 
 const envService = new EnvService<EnvVariables>(process.env);
--- src/services/notion/index.ts
+++ src/services/notion/index.ts
@@ -1,8 +1,9 @@
 import { envService } from '../env/index.js';
+import { fetch } from '../http/http.service.js';
 import { NotionApi } from './notion.api.js';
 
 const { BASE_API, TOKEN } = envService.variables;
 
-const notionApi = new NotionApi(BASE_API, TOKEN);
+const notionApi = new NotionApi(fetch, BASE_API, TOKEN);
 
 export { notionApi };
